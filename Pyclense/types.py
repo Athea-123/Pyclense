f1c3a7c7993798e7c511c47db17c0b80efa7a0d8
Kate M. Avanceña <kateavanmarfe@gmail.com>
2025-11-19 00:26:59 +0800

    Update types.py

--- Pyclense/types.py
+++ Pyclense/types.py
@@ -6,6 +6,7 @@ class TypeCleaner:
 
   def object_to_int(self, column):
     # errors= 'coerce' turns text like 'missing' or 'N/A' into NaN
+    self.df[column] = pd.to_numeric(self.df[column], errors='coerce')
     return self.df
 
   def object_to_datetime(self, column):
