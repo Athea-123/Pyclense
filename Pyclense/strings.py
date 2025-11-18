0828d2d8080cf06cff56387571d21a62853bb6b8
Kate M. Avanceña <kateavanmarfe@gmail.com>
2025-11-19 00:12:39 +0800

    Update strings.py

--- Pyclense/strings.py
+++ Pyclense/strings.py
@@ -1,1 +1,21 @@
+import pandas as pd 
 
+class StringCleaner: 
+  def __init__(self, df):
+    self.df = df
+
+  def to_lowercase(self, column):
+    self.df[column] = self.df[column].str.lower()
+    return self.df
+
+  def to_uppercase(self, column):
+    self.df[column] = self.df[column].str.upper()
+    return self.df
+
+  def to_titlecase(self, column):
+    self.df[column] = self.df[column].str.title()
+    return self.df
+
+  def strip_whitespace(self, column):
+    self.df[column] = self.df[column].str.strip()
+    return self.df
