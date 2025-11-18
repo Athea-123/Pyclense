43bfb5c23a7501fb83996d43968849efd878c981
Kate M. Avanceña <kateavanmarfe@gmail.com>
2025-11-19 00:16:57 +0800

    Update types.py

--- Pyclense/types.py
+++ Pyclense/types.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+class TypeCleaner:
+  def __init__(self, df):
+    self.df = df
+
+  def object_to_init(self, column):
+    # errors= 'coerce' turns text like 'missing' or 'N/A' into NaN
+    return self.df
+
+  def object_to_datetime(self, column):
+    self.df[column] = pd.to_datetime(self.df[column], errors='coerce')
+    return self.df
+
+  def object_to_float(self, column):
+    self.df[column] = pd.to_numeric(self.df[column], downcast='float', errors='coerce')
+    return self.df
